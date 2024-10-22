a57eeda731374161f63c565c59e459bde17f71d4
tazi <ayoub.vinci@student.vinci.be>
2024-10-22 14:12:39 +0200

    new:ex2.5-destructuring

---
+++ exercices/exerciceWEB/2.5/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+
+import ClickCounter from '../ClickCounter'
+
+
+
+function App() {
+  
+
+  return (
+
+    <ClickCounter title="bienvenue " message="You are a master in the art of clicking !" />
+   
+  )
+}
+
+export default App
